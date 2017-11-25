e92ada3ab75a62988187657963f3c960c9acf1fe
pei wang <519875872@qq.com>
2017-11-25 12:10:30 +0800

    change the folder name to chapter-1

--- chapter1-bk/src/test/resources/sql/customer_init.sql
+++
@@ -1,3 +0,0 @@
-TRUNCATE customer;
-INSERT INTO customer (name, contact, telephone, email, remark) VALUES ('customer1','Jack','13512345678','jack@gmail.com', null);
-INSERT INTO customer (name, contact, telephone, email, remark) VALUES ('customer2','Rose','13512343278','Rose@gmail.com', null);
